9d8d29839cf8c54de295c6ca93d797d19a3c6ea6
jannat28-349 <jucse2.349@gmail.com>
2023-06-03 02:29:08 +0600

    done image vault

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      colors: {
+        topNavColor: "#232228",
+        subNavColor: "#35353d",
+        bodyColor: "#3c3c44",
+        textColor: "#FFF",
+        grayColor: "#00000020",
+        blackColor: "#000",
+      },
+    },
+  },
+  plugins: [],
+};
